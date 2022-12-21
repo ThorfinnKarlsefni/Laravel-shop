a97141e7cd226ac963e5c13276de7bb8f817c48b
Cheung <402832626@qq.com>
2022-12-21 21:23:35 +0800

    update address

--- app/Http/Controllers/UserAddressController.php
+++ app/Http/Controllers/UserAddressController.php
@@ -8,19 +8,22 @@ use Illuminate\Http\Request;
 
 class UserAddressController extends Controller
 {
-    public function index(Request $request){
-        return view('user_addresses.index',[
-            'addresses' =>  $request->user()->addresses
-         ]);
+    public function index(Request $request)
+    {
+        return view('user_addresses.index', [
+            'addresses' => $request->user()->addresses
+        ]);
     }
 
-    public function create(){
-        return view('user_addresses.create_and_edit',[
-            'address'   =>  new UserAddress()
-            ]);
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', [
+            'address' => new UserAddress()
+        ]);
     }
 
-    public function store(UserAddressRequest $request){
+    public function store(UserAddressRequest $request)
+    {
         $request->user()->addresses()->create($request->only([
             'province',
             'city',
@@ -29,6 +32,28 @@ class UserAddressController extends Controller
             'zip',
             'contact_name',
             'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    public function edit(UserAddress $user_address)
+    {
+        return view('user_addresses.create_and_edit', [
+            'address' => $user_address
+        ]);
+    }
+
+    public function update(Request $request, UserAddress $user_address)
+    {
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone'
         ]));
 
         return redirect()->route('user_addresses.index');
