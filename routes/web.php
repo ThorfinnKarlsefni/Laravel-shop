a8c83e078a40e5c2679418cab211b886ade86c70
Cheung <402832626@qq.com>
2022-12-21 19:48:30 +0800

    verified mail

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','PagesController@root')->name('root');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes(['verify' => true]);
