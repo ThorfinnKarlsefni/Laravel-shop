c95f0da95b1291779c74722c77e197e4960de0ea
Cheung <402832626@qq.com>
2022-12-28 15:58:41 +0800

    user - order detail page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
@@ -29,8 +30,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart','CartController@index')->name('cart.index');
     Route::delete('cart/{sku}','CartController@remove')->name('cart.remove');
-    Route::post('orders','OrdersController@store')->name('orders.store');
-    Route::get('orders','OrdersController@index')->name('orders.index');
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
+    Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
 });
 
 Route::redirect('/', '/products')->name('root');
