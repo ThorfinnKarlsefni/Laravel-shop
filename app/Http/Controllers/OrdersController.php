d73bb9bb8148c97905f2ce482d857ba1ff44869c
Cheung <402832626@qq.com>
2022-12-28 18:07:23 +0800

    encapsulation code

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,76 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
 use App\Http\Requests\Request;
-use App\Jobs\CloseOrder;
 use App\Models\Order;
-use App\Models\ProductSku;
-use Carbon\Carbon;
+use App\Services\OrderService;
 use App\Models\UserAddress;
 
 class OrdersController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $orders = Order::query()
-            ->with(['items.product','items.productSku'])
-            ->where('user_id',$request->user()->id)
-            ->orderBy('created_at','desc')
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
             ->paginate();
 
-        return view('orders.index',['orders' => $orders]);
+        return view('orders.index', ['orders' => $orders]);
     }
 
     public function show(Order $order, Request $request)
     {
-        $this->authorize('own',$order);
+        $this->authorize('own', $order);
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
-    public function store(OrderRequest $request){
+
+    public function store(OrderRequest $request, OrderService $orderService)
+    {
         $user = $request->user();
-        $order = \DB::transaction(function () use ($user,$request){
-            $address = UserAddress::find($request->input('address_id'));
-            $address->update(['last_used_at' => Carbon::now()]);
-            $order = new Order([
-                'address' => [
-                    'address'   =>  $address->full_address,
-                    'zip'   => $address->zip,
-                    'contact_name'  =>  $address->contact_name,
-                    'contact_phone' =>  $address->contact_phone
-                ],
-                'remark'    => $request->input('remark'),
-                'total_amount' => 0,
-            ]);
-            $order->user()->associate($user);
-            $order->save();
-            $totalAmount = 0;
-            $items = $request->input('items');
-            foreach ($items as $data) {
-                $sku = ProductSku::find($data['sku_id']);
-                $item = $order->items()->make([
-                    'amount'    => $data['amount'],
-                    'price' => $sku->price,
-                ]);
+        $address = UserAddress::find($request->input('address_id'));
 
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku);
-                $item->save();
-                $totalAmount += $sku->price * $data['amount'];
-                if($sku->decreaseStock($data['amount']) <= 0){
-                    throw new InvalidRequestException('该商品库存不足');
-                }
-            }
-
-            $order->update(['total_amount' => $totalAmount]);
-
-            $skuIds = collect($items)->pluck('sku_id');
-            $user->cartItems()->whereIn('product_sku_id',$skuIds)->delete();
-
-            return $order;
-        });
-
-        $this->dispatch(new CloseOrder($order,config('app.order_ttl')));
-        return $order;
+        return $orderService->store($user, $address, $request->input('address_id'), $request->input('items'));
     }
 }
