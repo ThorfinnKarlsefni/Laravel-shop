b50c372d52a8340ba1aaa414ffc04ee7ef91ecd1
Cheung <402832626@qq.com>
2022-12-29 20:17:37 +0800

    integration ngrok

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,11 @@ function test_helper(){
 function route_class(){
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function ngrok_url($routeName,$parameters = []){
+    if(app()->environment('local') && $url = config('app.ngrok_url')){
+        return $url.route($routeName,$parameters,false);
+    }
+
+    return route($routeName,$parameters);
+}
