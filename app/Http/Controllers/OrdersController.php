f7f761fdd1f851abe59d7ca4f6c730dc8be9a95f
Cheung <402832626@qq.com>
2022-12-31 19:34:45 +0800

    about confirm receipt

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
 use App\Http\Requests\Request;
 use App\Models\Order;
 use App\Services\OrderService;
 use App\Models\UserAddress;
-use Encore\Admin\Controllers\AdminController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-class OrdersController extends AdminController
+class OrdersController extends Controller
 {
     use ValidatesRequests;
     public function index(Request $request)
@@ -36,6 +36,17 @@ class OrdersController extends AdminController
         $address = UserAddress::find($request->input('address_id'));
 
         return $orderService->store($user, $address, $request->input('address_id'), $request->input('items'));
+    }
+
+    public function received(Order $order){
+        $this->authorize('own',$order);
+
+        if($order->ship_status !== Order::SHIP_STATUS_DELIVERED){
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+        return $order;
     }
 
 }
