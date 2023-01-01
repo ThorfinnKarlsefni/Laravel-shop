1f8968caa2a467d381b4ef2e35620aaf18e7d4e1
Cheung <402832626@qq.com>
2023-01-01 23:47:16 +0800

    admin - order disagree refund

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Http\Requests\Request;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
@@ -152,5 +153,25 @@ class OrdersController extends AdminController
         ]);
 
         return redirect()->back();
+    }
+
+    public function handleRefund(Order $order,HandleRefundRequest $request){
+        if($order->refund_status !== Order::REFUND_STATUS_APPLIED){
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        if($request->input('agree')){
+
+        }else {
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_PENDING,
+                'extra'     => $extra,
+            ]);
+        }
+
+        return $order;
     }
 }
