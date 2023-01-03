8924557c3644982f38188a31a6b4bf6ab62cf8f4
Cheung <402832626@qq.com>
2023-01-03 15:52:19 +0800

    optimized coupon module

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -9,11 +9,11 @@ use Illuminate\Http\Request;
 
 class CouponCodesController extends Controller
 {
-    public function show($code){
+    public function show($code,Request $request){
         if(!$record = CouponCode::where('code',$code)->first()){
             throw new CouponCodeUnavailableException('优惠卷不存在');
         }
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $record;
     }
