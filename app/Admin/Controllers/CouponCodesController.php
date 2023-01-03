d76b436c37c2c98816759b55e4425a60e2dd9e64
Cheung <402832626@qq.com>
2023-01-03 13:11:14 +0800

    admin - update、destroy couponcode

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -72,7 +72,13 @@ class CouponCodesController extends AdminController
 
         $form->display('id', 'Id');
         $form->text('name', '名称')->rules('required');
-        $form->text('code', '优惠码')->rules('nullable|unique:coupon_codes');
+        $form->text('code', '优惠码')->rules(function ($form){
+            if ($id = $form->model()->id){
+                return 'nullable|unique:coupon_codes,code,'.$id.',id';
+            }else{
+                return 'nullable|unique:coupon_codes';
+            }
+        });
         $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required')->default(CouponCode::TYPE_FIXED);
         $form->text('value', '折扣')->rules(function ($form) {
             if (request()->input('type') === CouponCode::TYPE_PERCENT) {
