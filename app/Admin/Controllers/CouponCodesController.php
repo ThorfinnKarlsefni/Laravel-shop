8d17c0bf93452ec918988f82fbf4fdb2b2ae2031
Cheung <402832626@qq.com>
2023-01-03 12:46:33 +0800

    admin - add coupon

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -31,7 +31,7 @@ class CouponCodesController extends AdminController
         $grid->id('ID')->sortable();
         $grid->name('名称');
         $grid->code('优惠码');
-        $grid->column('description','描述');
+        $grid->column('description', '描述');
         $grid->column('usage', '用量')->display(function ($value) {
             return "{$this->used} / {$this->total}";
         });
@@ -58,7 +58,6 @@ class CouponCodesController extends AdminController
         $show = new Show(CouponCode::findOrFail($id));
 
 
-
         return $show;
     }
 
@@ -71,7 +70,29 @@ class CouponCodesController extends AdminController
     {
         $form = new Form(new CouponCode());
 
+        $form->display('id', 'Id');
+        $form->text('name', '名称')->rules('required');
+        $form->text('code', '优惠码')->rules('nullable|unique:coupon_codes');
+        $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required')->default(CouponCode::TYPE_FIXED);
+        $form->text('value', '折扣')->rules(function ($form) {
+            if (request()->input('type') === CouponCode::TYPE_PERCENT) {
+                return 'required|numeric|between:1,99';
+            } else {
+                return 'required|numeric|min:0.01';
+            }
+        });
 
+        $form->text('total', '总量')->rules('required|numeric|min:0');
+        $form->text('min_amount', '最低金额')->rules('required|numeric|min:0');
+        $form->datetime('not_before', '开始时间');
+        $form->datetime('not_after', '结束时间');
+        $form->radio('enabled', '启用')->options(['1' => '是', '0' => '否']);
+
+        $form->saving(function (Form $form) {
+            if (!$form->code) {
+                $form->code = CouponCode::findAvailableCode();
+            }
+        });
 
         return $form;
     }
